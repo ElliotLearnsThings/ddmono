9b5843fdd9120904179b7da784192451e1727438
CormAlan <cormacalanbrown@gmail.com>
2025-09-23 11:25:45 +0200

    4

---
+++ DD1320LAB4/kth_tilda_binarsokning.py
@@ -0,0 +1,29 @@
+from typing import List, Any
+
+def binary_search(a: List[Any], x: Any) -> int:
+    """Returnerar index för x i a, eller -1 om x saknas."""
+    lo, hi = 0, len(a) - 1
+    while lo <= hi:
+        mid = (lo + hi) // 2
+        if a[mid] < x:
+            lo = mid + 1
+        elif a[mid] > x:
+            hi = mid - 1
+        else:
+            return mid
+    return -1
+
+def main():
+    indata = input().strip()
+    the_list = indata.split()
+
+    key = input().strip()
+    while key != "#":
+        idx = binary_search(the_list, key)
+        if idx == -1:
+            print(None)
+        else:
+            print(the_list[idx])
+        key = input().strip()
+
+main()
